c41f507122ff2ebbd98b550cfff11a93197ef4fe
preethi <selvampreethi360@gmail.com>
2024-02-01 13:08:27 +0530

    change a h1 style

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,6 @@
 const Body = () => {
   return (
-    <h1 className="text-2xl font-bold capitalize text-center">
-      Welcome to Redux store
-    </h1>
+    <h1 className="text-4xl capitalize text-center">Welcome to Redux store</h1>
   );
 };
 
